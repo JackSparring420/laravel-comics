0de5a0be1e1169e78ba3ae21231848a4ff3ac3aa
JackSparring420 <emanuele.arconte94@gmail.com>
2022-01-21 17:11:33 +0100

    fixed word comics in header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,7 +3,7 @@
     <nav>
         <ul>
             <li> <a href="">CHARACTER</a> </li>
-            <li class="active"><a href="{{ route('home')}}">COMICS </a> </li>
+            <li ><a href="{{ route('home')}}" class="active">COMICS </a> </li>
             <li> <a href="">MOVIES</a> </li>
             <li> <a href="">TV</a> </li>
             <li> <a href="">GAMES</a> </li>
